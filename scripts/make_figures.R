077b88051475669b3081d022fad672f94a22c7a6
vshanka23 <vshanka@clemson.edu>
2024-02-22 15:37:49 -0500

    Spencer Hatfield found a bug in make_figures.R where ylim hard coded at 10 was omitting some highly significant signals from the Manhattan plot. His solution is an if-then statement to adjust ylim if higher than -log(p) of 10.

--- scripts/make_figures.R
+++ scripts/make_figures.R
@@ -8,7 +8,10 @@ png(output_qq_png)
 qq(new3$P)
 dev.off()
 png(output_man_png)
-manhattan(new3, main = "Manhattan Plot", ylim = c(0, 10), cex = 0.6, cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = F, genomewideline = F, chrlabs = c("2L","2R","3L","3R","4","MT","X","Y"))
+#Spencer Hatfield identified some associations where some p-values were < 1E-10. These were being omitted because of the hard coded ylim in the plot. His solution is below:
+max_p <- max(-log10(new3$P))
+ylim_max <- ifelse(max_p > 10, max_p, 10)
+manhattan(new3, main = "Manhattan Plot", ylim = c(0, ylim_max), cex = 0.6, cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = F, genomewideline = F, chrlabs = c("2L","2R","3L","3R","4","MT","X","Y"))
 dev.off()
 }
 
